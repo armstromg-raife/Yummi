85f18c3ab5ab7bd58a3d950e110f78807bb01765
armstromg-raife <armstrong.raife@etoncollege.org.uk>
2024-02-27 11:17:21 +0000

    recipe navigaiton view

---
+++ Yummi/view/RecipeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailView.swift
+//  Yummi
+//
+//  Created by Armstrong, Raife (SPH) on 27/02/2024.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailView()
+}
